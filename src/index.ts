1361e839608c79e3846cb5219dbf2c15a07be0b9
João Martins <jvsm123@outlook.com>
2023-06-13 12:47:35 -0300

    docs: updated some refs and others things

--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,18 @@ import swaggerUI from "swagger-ui-express";
 import swaggerDocument from "../swagger.json";
 
 config();
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 app.use(routes);
-app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+
+app.use(
+  "/api-docs",
+  swaggerUI.serve,
+  swaggerUI.setup(swaggerDocument, { explorer: true })
+);
 
 app.listen(3000, () =>
   console.log("server is running in http://localhost:3000")
